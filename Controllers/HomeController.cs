da0cc332f3ecbd1ec100065b6ca27ff93327907c
Paulo Gustavo <pauloestudando159@gmail.com>
2023-11-25 15:49:45 -0300

    feat - Listando todas as cidades

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,8 +9,20 @@ namespace StudyingProcedures.Controllers
     [Route("")]
     public class HomeController : ControllerBase
     {
-       
+        private readonly CitiesContext _context;
 
-            
+        public HomeController(CitiesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("todas")]
+        public async Task<List<Cities>> getAll()
+        {
+
+            return await _context.cities.FromSqlRaw("EXEC GetAll").ToListAsync();
+        }
+
+
     }
 }
